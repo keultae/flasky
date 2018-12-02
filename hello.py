8243e4cf3c2150b9100731b6607858a4f4766264
keultae <keultae@korea.com>
2018-12-02 21:12:04 +0900

    app.run(debug=True) 추가

--- hello.py
+++ hello.py
@@ -11,21 +11,17 @@ app.config['SECRET_KEY'] = 'hard to guess string'
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
-
 class NameForm(FlaskForm):
     name = StringField('What is your name?', validators=[DataRequired()])
     submit = SubmitField('Submit')
-
 
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404
 
-
 @app.errorhandler(500)
 def internal_server_error(e):
     return render_template('500.html'), 500
-
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -37,3 +33,6 @@ def index():
         session['name'] = form.name.data
         return redirect(url_for('index'))
     return render_template('index.html', form=form, name=session.get('name'))
+
+if __name__ == '__main__':
+    app.run(debug=True)
